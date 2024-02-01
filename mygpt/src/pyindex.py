a4ab38f68ae1e5e0a144521777a6026ffaf86e44
Anton Suslonov <toshick@hotmail.com>
2024-02-01 13:23:22 +0200

    dirs

---
+++ mygpt/src/pyindex.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from mygpt import application
+
+if __name__ == "__main__":
+    application.run()
+
+
