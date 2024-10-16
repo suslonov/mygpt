f0394e60a835f7212eba8e80e130f144121ba410
Julia <netoshick@gmail.com>
2024-10-16 17:13:12 +0300

    id fix

--- mygpt/src/mygpt.py
+++ mygpt/src/mygpt.py
@@ -65,7 +65,7 @@ def _home_page(uuid):
                 if "content" in m:
                     m["content_list"] = m["content"].split("\n")
         return render_template('home-page-mygpt.html',
-                               sub_path = sub_path + (secret_url if secret_url else "") + ("/id/" + uuid if uuid else ""),
+                               sub_path = (secret_url if secret_url else "") + ("/id/" + uuid if uuid else ""),
                                greetings = GREETINGS,
                                session_id = sessionid,
                                messages = messages)
@@ -85,7 +85,7 @@ def _home_page(uuid):
             sessionid = md5(sessionstr.encode("utf8")).hexdigest()
             messages = []
         return render_template('home-page-mygpt.html',
-                                sub_path = sub_path + (secret_url if secret_url else "") + ("/id/" + uuid if uuid else ""),
+                               sub_path = ("/id/" + uuid if uuid else ""),
                                greetings = GREETINGS,
                                session_id = sessionid,
                                messages = messages)
